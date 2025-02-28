2db43c16157ea042cb6218154c278f847d761cab
MathisMM <mathis-morales@outlook.fr>
2025-02-27 18:47:14 -0600

    Create noise_classifier.py

---
+++ noise_classifier.py
@@ -0,0 +1,21 @@
+#-----------------------------------------------
+# Author : Mathis Morales                       
+# Email  : mathis-morales@outlook.fr             
+# git    : https://github.com/MathisMM            
+#-----------------------------------------------
+
+import os 
+import numpy as np
+import pandas as pd
+
+import torch
+from torch import nn
+from torchsummary import summary
+
+sensor_list = ['CAM_BACK','CAM_BACK_LEFT','CAM_BACK_RIGHT','CAM_FRONT','CAM_FRONT_LEFT','CAM_FRONT_RIGHT',
+                'RADAR_FRONT','RADAR_FRONT_LEFT','RADAR_FRONT_RIGHT','RADAR_BACK_LEFT','RADAR_BACK_RIGHT']
+
+cam_list = ['CAM_BACK','CAM_BACK_LEFT','CAM_BACK_RIGHT','CAM_FRONT','CAM_FRONT_LEFT','CAM_FRONT_RIGHT']
+
+radar_list = ['RADAR_FRONT','RADAR_FRONT_LEFT','RADAR_FRONT_RIGHT','RADAR_BACK_LEFT','RADAR_BACK_RIGHT']
+
