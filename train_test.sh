a00589e9e84e2e02ba0f6c961d61ffeac89741ad
Mathis <mathis.morales@ou.edu>
2025-02-28 22:27:45 -0600

    bash launcher

---
+++ train_test.sh
@@ -0,0 +1,12 @@
+python noise_classifier_cam.py
+
+git add cam_model_hist.pkl
+git commit -m 'history for camera noise classifier'
+git push
+
+
+python noise_classifier_radar.py
+
+git add radar_model_hist.pkl
+git commit -m 'history for radar noise classifier'
+git push
