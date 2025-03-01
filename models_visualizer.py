a09253ac5678f838a8e9e6288343d782a529e07f
Mathis <mathis.morales@ou.edu>
2025-02-28 22:28:38 -0600

    added neural net viz functions

---
+++ models_visualizer.py
@@ -0,0 +1,26 @@
+# #-----------------------------------------------
+# # Author : Mathis Morales                       
+# # Email  : mathis-morales@outlook.fr             
+# # git    : https://github.com/MathisMM            
+# #-----------------------------------------------
+
+import os 
+import numpy as np
+import pandas as pd
+import cv2
+import matplotlib.pyplot as plt
+
+# Neural Net plots
+def plot_trainval_loss(history):
+    fig, ax = plt.subplots(1,1,figsize=(16,9))
+
+    pass
+
+def plot_trainval_acc(history):
+    pass
+
+def plot_test_acc(history):
+    pass
+
+def confusion_mat(history):
+    pass
