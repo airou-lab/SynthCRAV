10c83789ad61a64ef6f9f1c78dfdce886c5d9cda
Mathis <mathis.morales@ou.edu>
2025-03-04 13:55:55 -0600

    cleanup

--- torchtest.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-if torch.cuda.is_available:
-    device = torch.device('cuda')
-    ndevice = torch.cuda.current_device()
-    print('found CUDA device:', torch.cuda.get_device_name(ndevice))
-else:
-    print('no CUDA installation found, using CPU')
